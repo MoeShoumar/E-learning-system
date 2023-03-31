777f74c97726037cce4753fa92d3be2f18b4cfd9
MoeShoumar <mks38@mail.aub.edu>
2023-04-01 00:41:25 +0300

    added register function

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,2 +1,20 @@
-const login = async () => {};
-const register = async () => {};
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
+
+const register = async (req, res) => {
+  const { email, name, password } = req.body;
+  const existingUser = await User.findOne({ email });
+  if (existingUser)
+    return res.status(409).json({ message: "email already exists" });
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+  const user = new User();
+  user.email = email;
+  user.password = hashedPassword;
+  user.name = name;
+
+  user.save();
+  res.status(201).json(user);
+};

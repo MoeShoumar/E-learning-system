8dc6453c2a0278cfd6a011cea3d812c9f91c3ee0
MoeShoumar <mks38@mail.aub.edu>
2023-03-31 18:16:31 +0300

    added login functionality

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -11,6 +11,43 @@ import "../../pages/Signin_signup/Signin_signup";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  let nav = useNavigate();
+  const handleSignin = async (e) => {
+    e.preventDefault();
+    const response = await loginUser({
+      email,
+      password,
+    });
+    if ("token" in response) {
+      var user = jwt_decode(response.token);
+      // localStorage.setItem("user_id", user._id);
+      // localStorage.setItem("username", user.username);
+
+      try {
+        localStorage.setItem("token", response["token"]);
+        if (user.user_type === 1) {
+          toast.success(`You Are Now Logged in.`);
+          nav("/doctor");
+        } else {
+          toast.success(`You Are Now Logged in.`);
+          nav("/patient");
+        }
+        setEmail("");
+        setPassword("");
+      } catch {
+        toast.error("Failed.");
+      }
+    } else {
+      toast.error("You are not Authorized!");
+    }
+  };
+  const loginUser = async (credentials) => {
+    // url = "/";
+    // data_signin = credentials;
+    // const response = await postAPI(url, data_signin);
+  };
   return (
     <div className="form-container sign-in-container">
       <form>

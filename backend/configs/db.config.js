c329a2d1cc39fb7868ded6e6ed000d6df4b5d4ea
MoeShoumar <mks38@mail.aub.edu>
2023-03-31 22:25:54 +0300

    setting up nodejs

---
+++ backend/configs/db.config.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: flase,
+});
+
+const db = mongoose.connection;
+
+db.on("error", (err) => console.error(err));
+db.on("open", () => console.log("moongo db connected"));

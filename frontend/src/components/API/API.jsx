0c1b04a9fbf20b3daa0bcd19fb9e5402beb14936
MoeShoumar <mks38@mail.aub.edu>
2023-03-31 18:14:39 +0300

    added global post and get api component

---
+++ frontend/src/components/API/API.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+export const getAPI = async (api_url, token = null) => {
+  try {
+    const headers = {};
+    if (token) {
+      headers.Authorization = `Bearer ${token}`;
+    }
+    const response = await axios.get(api_url, { headers });
+    return response.data;
+  } catch (error) {
+    console.log("Error from GET API");
+  }
+};
+
+export const postAPI = async (api_url, api_data, token = null) => {
+  try {
+    return await axios.post(api_url, api_data, {
+      headers: {
+        Authorization: token,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};

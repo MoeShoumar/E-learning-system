a93a46170c842565db6921a1a47a87935bc77a4e
MoeShoumar <mks38@mail.aub.edu>
2023-04-01 00:41:41 +0300

    added login function

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -2,7 +2,19 @@ const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 const jwt = require("jsonwebtoken");
-
+const login = async (req, res) => {
+  const { email, password } = request.body;
+  const validuser = await User.findOne({ email });
+  const checkpassowrd = await bcrypt.compare(password, user.password);
+  if (checkpassowrd && validuser)
+    return res.status(200).json({ message: "login sucess" });
+  return res.status(400).json({ message: "invalid credintails" });
+  const token = jwt.sign(
+    { id: user._id, email: user.email },
+    process.env.SECRET_KEY
+  );
+  res.json({ token });
+};
 const register = async (req, res) => {
   const { email, name, password } = req.body;
   const existingUser = await User.findOne({ email });

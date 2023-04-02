6c5384ce65a1231e5fc8c97f5fbdccd2c53c4725
MoeShoumar <mks38@mail.aub.edu>
2023-04-02 12:16:49 +0300

    fixed major bug in index

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,22 @@
 const express = require("express");
-const cluster = require("cluster");
+// const cluster = require("cluster");
 const cors = require("cors");
-const mongoose = require("mongoose");
-cors();
 const app = express();
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 require("dotenv").config();
+
+const adminRoutes = require("./routes/admin.routes");
+app.use("/admin", adminRoutes);
+
+const authRoutes = require("./routes/auth.routes");
+app.use("/auth", authRoutes);
 
 app.listen(process.env.PORT, (err) => {
   if (err) console.log(err);
   console.log("Server is listining on port: ", process.env.PORT);
   require("./configs/db.config");
 });
-const adminRoutes = require("./routes/admin.routes");
-app.use("/admin", adminRoutes);
-
-const authRoutes = require("./routes/auth.routes");
-app.use("/auth", authRoutes);
 
 // const studentRoutes = require("./routes/student.routes");
 // const { authMiddleware } = require("./middlewares/auth.middleware");

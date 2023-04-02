09aa67bc8cf09f9a77189781896cc6e0899c3420
MoeShoumar <mks38@mail.aub.edu>
2023-04-02 10:39:26 +0300

    added auth middleware

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,17 +1,17 @@
-const jwt = require("jsonwebtoken");
-const User = require("../models/userModel");
+// const jwt = require("jsonwebtoken");
+// const User = require("../models/userModel");
 
-exports.authMiddleware = async (req, res, next) => {
-  try {
-    const token = req.headers.authorization?.split(" ")[1];
-    if (!token) {
-      return res.statu(403).json({ message: "unauthenticated" });
-    }
-    const decoded = jwt.verify(token, process.env.SECRET_KEY);
-    const user = User.findById(decoded.id, "-password");
-    // req.user = usser; we use it la yehmol l user to the end point
-    next();
-  } catch (error) {
-    return res.statu(500).json({ message: "Server error" });
-  }
-};
+// exports.authMiddleware = async (req, res, next) => {
+//   try {
+//     const token = req.headers.authorization?.split(" ")[1];
+//     if (!token) {
+//       return res.statu(403).json({ message: "unauthenticated" });
+//     }
+//     const decoded = jwt.verify(token, process.env.SECRET_KEY);
+//     const user = await User.findById(decoded.id, "-password");
+// req.user = usser; we use it la yehmol l user to the end point
+//     next();
+//   } catch (error) {
+//     return res.statu(500).json({ message: "Server error" });
+//   }
+// };

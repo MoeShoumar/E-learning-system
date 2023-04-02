704bdb2f69ff5189d0f689a98ac54ddfde643f48
MoeShoumar <mks38@mail.aub.edu>
2023-04-02 10:39:38 +0300

    added admin routes

---
+++ backend/routes/admin.routes.js
@@ -0,0 +1,16 @@
+const { authMiddleware } = require("../middlewares/auth.middleware");
+const { adminMiddleware } = require("../middlewares/admin.middleware");
+const { Router } = require("express");
+const {
+  getAllstudents,
+  handleFormRequest,
+  uploadFile,
+  creatCourse,
+} = require("../controllers/admin.controllers");
+
+const router = Router();
+// router.get("/", getAllstudents);
+// router.get("/:id", handleFormRequest);
+// router.post("/", uploadFile);
+router.post("/course", creatCourse);
+module.exports = router;

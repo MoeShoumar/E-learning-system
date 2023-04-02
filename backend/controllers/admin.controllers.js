a27a27f5e0fd7cb7c68511b609f58246665fa3b5
MoeShoumar <mks38@mail.aub.edu>
2023-04-02 14:01:13 +0300

    finished get pending request api

--- backend/controllers/admin.controllers.js
+++ backend/controllers/admin.controllers.js
@@ -7,7 +7,26 @@ exports.creatCourse = async (req, res) => {
   const course = await Course.create({ name, CRN });
   return res.json({ message: "sucess", course });
 };
+// exports.handledropRequest = async (req,res){
 
+// }
+
+exports.getrequests = async (req, res) => {
+  try {
+    const course = await Course.findOne({
+      pendingRequests: { $exists: true, $not: { $size: 0 } },
+    });
+    if (!course) {
+      return res
+        .status(404)
+        .json({ message: "No courses with pending requests found" });
+    }
+    return res.status(200).json({ course });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
 // model.findoneAndupdate to remove class from student
 // file.findOneAndUpdate to
 
--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -3,14 +3,16 @@ const { adminMiddleware } = require("../middlewares/admin.middleware");
 const { Router } = require("express");
 const {
   getAllstudents,
-  handleFormRequest,
+  handledropRequest,
   uploadFile,
+  getrequests,
   creatCourse,
 } = require("../controllers/admin.controllers");
 
 const router = Router();
 // router.get("/", getAllstudents);
-// router.get("/:id", handleFormRequest);
+router.get("/adminrequest", getrequests);
+// router.post("/droprequest", handledropRequest);
 // router.post("/", uploadFile);
 router.post("/course", creatCourse);
 
